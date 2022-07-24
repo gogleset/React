f1d4a363309ebc38b94048e17a32e89fbe5f3294
choi_j <choij2494@gmail.com>
2022-07-24 12:22:54 +0900

    add: proxy setup

--- map-project/src/Data/API/GetWeatherAPI.js
+++ map-project/src/Data/API/GetWeatherAPI.js
@@ -1,5 +1,12 @@
 import axios from "axios";
+import config from "../../config.js";
+import DayHelper from "../../Helper/DayHelper.js";
+const Day = new DayHelper();
+const today = Day.getDay(); //현재 날짜
+const hour = Day.getHour(); //현재 시간
+const minute = Day.getMinute(); //현재 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     // 실황데이터를 가져옵니다.
     getLiveSitualtion: async (nx, ny) => {
@@ -9,17 +16,21 @@ export default {
                     nx: nx, ny: ny
                 }
             });
+            return {data: data}
         } catch (err) {
             console.log(err);
         }
     },
     getLiveForecast: async (nx, ny) => {
+        let forecastMinutes = minute;
+        if (Number(forecastMinutes) < 30) {
+            forecastMinutes = "00";
+        } else if (Number(forecastMinutes) > 60 && Number(forecastMinutes) > 30) {
+            forecastMinutes = "30";
+        }
         try {
-            const data = await axios.get('/api/weather/liveForecast', {
-                params: {
-                    nx: nx, ny: ny
-                }
-            });
+            console.log('/api' + `${config.weatherUrls}${config.liveForcast}ServiceKey=${config.keys.weatherEncodingKey}&pageNo=1&numOfRows=60&dataType=JSON&base_date=${today}&base_time=${hour}${forecastMinutes}&nx=${nx}&ny=${ny}`)
+            const data = await axios.get('/api' + `${config.weatherUrls}${config.liveForcast}ServiceKey=${config.keys.weatherEncodingKey}&pageNo=1&numOfRows=60&dataType=JSON&base_date=${today}&base_time=${hour}${forecastMinutes}&nx=${nx}&ny=${ny}`);
             console.log("try");
             return { data: data };
         } catch (err) {
